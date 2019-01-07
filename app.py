011babdf743630299ce8a0a9807c612f65b64e7d
amaanooo <md.amanuddin@gmail.com>
2019-01-06 20:26:36 -0500

    complete

--- app.py
+++ app.py
@@ -8,24 +8,23 @@ class User(dbase.Model):
     id=dbase.Column(dbase.Integer,primary_key=True)
     name=dbase.Column(dbase.String(255)) # It will help in creating database in the sqlite database
     def __repr__(self):
-        return '<Name %r>' % self.name 
+        return '<Name %r>' % self.name #if you print this object it will output the name and not the id
 
 
-@application.route('/hello',methods=['GET','POST'])
+@application.route('/hello')
 def hello():
-    if(request.method =='POST'):
-        userinput= request.get_json()
-        return jsonify({'You typed':userinput}),202 #I set response code to 202
-    else:
-        return jsonify({'You are': 'Stranger'})
-@application.route('/hello/<string:var>',methods=['GET'])
+    #if(request.method =='POST'):
+        #userinput= request.get_json()
+        #return jsonify({'You typed':userinput}),202 #I set response code to 202
+    #else:
+    return jsonify('Hello Stranger!')
+@application.route('/hello/<string:var>')
 def retr(var):
     vr=User(name=var)
     dbase.session.add(vr)   #write the name to the database
-    dbase.session.commit()
-    us=User.query.all()
-    print(us)
-    return jsonify({'Hello':var}) #return the string entered at the RESSFULL endpoint as JSON data
+    dbase.session.commit()  #commit the string name to database
+    var1='hello'+' '+var
+    return jsonify(var1) #return the string entered at the RESSFULL endpoint as JSON data
 
 if  __name__ == '__main__':
     application.run(debug=True, host='0.0.0.0')
