5f4dde1bddf1c14730a99b52bb05daa5eeeacfd8
amaanooo <md.amanuddin@gmail.com>
2019-01-07 12:08:27 -0500

    final

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def index():
         #userinput= request.get_json()
         #return jsonify({'You typed':userinput}),202 #I set response code to 202
     #else:
-    return "<h3> Usage: http://localhost/hello/<somestring> </h3>"
+    return "<h3> Usage: http://localhost:5000/hello/somethingOrnothing </h3>"
 
 @application.route('/hello')
 def hello():
--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ echo "enter name of docker image that you want"
 read name_image
 sudo apt-get install docker.io
 sudo docker build -t $name_image .
-sudo docker run -it -p 80:5000 $name_image
+sudo docker run -it -p 5000:5000 $name_image
